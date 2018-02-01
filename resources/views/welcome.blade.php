db1b2cf9a44a45c7c74fe5a4ba58b03170f87334
davidcrc <david_drcr@hotmail.com>
2018-02-01 12:55:34 -0500

    07. Crea Layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,102 +1,29 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')     {{--  Creando layouts , extends, section  --}}
 
-        <title>Laravel by David</title>
+@section('content')
+    <div class="title m-b-md">
+        Laravel by <a href="http://www.asiesps.com"> DaviD </a>
+    </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    {{--  Video 05  --}}
+    @if(isset($teacher))
+        <a> Profesor : {{$teacher}} </a>
+    @else
+            Profe a definir ...
+    @endif
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <div class="links">
+        
+        @foreach ($linkss as $link => $text)
+        <a href="{{ $link }}" > {{$text}} </a>
+        @endforeach
 
-            .full-height {
-                height: 100vh;
-            }
+    </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+@endsection
 
-            .position-ref {
-                position: relative;
-            }
+{{-- 06 : Si no se define un yield('title'), entonces carga este de aqui  --}}
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel by DaviD
-                </div>
-
-                {{--  Video 05  --}}
-                @if(isset($teacher))
-                    <a> Profesor : {{$teacher}} </a>
-                @else
-                        Profe a definir ...
-                @endif
-
-                <div class="links">
-                    
-                    @foreach ($linkss as $link => $text)
-                    <a href="{{ $link }}" > {{$text}} </a>
-                    @endforeach
-
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+{{--  @section('title')
+Laravel by David
+@endsection  --}}
